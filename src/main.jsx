3c7fdded7d5e10f786c0a13a61e4cf92c4f8b595
guilherme soares <guilherme.dev36@gmail.com>
2023-01-31 14:54:34 -0300

    feat: utils, remove defalt react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { People } from "./pages/PeopleDetails";
+import { Search } from "./pages/Search";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route exact  path="/" element={<Home />} />
+          <Route path="/people/:id" element={<People/>} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
